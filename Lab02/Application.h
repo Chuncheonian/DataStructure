579d20f0baade63b66d19bf545362d71874187a2
Chuncheonian <ehddud2468@khu.ac.kr>
2020-11-22 23:15:21 +0900

    Add Lab02

---
+++ Lab02/Application.h
@@ -0,0 +1,146 @@
+#pragma once
+#include  <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
+
+#include "ItemType.h"
+#include "SortedList.h"
+
+#define FILENAMESIZE 1024
+
+/**
+ * Application class for item management simply.
+ */
+class Application{
+private:
+    ifstream        m_InFile;       // input file descriptor.
+    ofstream        m_OutFile;      // output file descriptor.
+    int             m_Command;      // current command number.
+    
+    SortedList      m_List;         // content list.
+
+public:
+    // default constructor.
+    Application() {
+        m_Command = 0;
+    }
+    
+    // default destructor.
+    ~Application() {}
+
+    /**
+     * @brief   Program driver.
+     * @pre     Program is started
+     * @post    Program is finished.
+     */
+    void Run();
+
+    /**
+     * @brief   Display command on screen and get a input from keyboard.
+     * @pre     None.
+     * @post    None.
+     * @return  User's command.
+     */
+    int GetCommand();
+
+    /**
+     * @brief   Add new record into sorted list.
+     * @pre     List should be initialized.
+     * @post    New record is added into the sorted list.
+     */
+    void AddContent();
+
+    /**
+     * @brief   Get a PK from keyboard and delete the rocord with same PK.
+     * @pre     List should be initialized.
+     * @post    Record is deleted into the list.
+     */
+    void DeleteContent();
+    
+    /**
+     * @brief   Get a Primary Key from keyboard and Replace the rocord with same Primary Key.
+     * @pre     List should be initialized.
+     * @post    Record is replaced into the list.
+     * @return  Return 1 if this function works well,
+     *          Oterwise return 0.
+     */
+    int ReplaceContent();
+
+    /**
+     * @brief   Searching for same content Name by Sequential search and Get the data.
+     * @pre     none
+     * @post    Print a record.
+     */
+    void SearchByName_SeqS();
+    
+    /**
+     * @brief   Searching for same content Name by Binary search and Get the data.
+     * @pre     none
+     * @post    Print a record.
+     */
+    void SearchByName_BinS();
+
+    /**
+     * @brief   Get a content name from keyboard and display a record with input content name. 
+     * @pre     Add data.
+     * @post    Content name에 해당하는 레코드를 출력
+     * @return  Return 1 if this function works well,
+     *          Oterwise return 0.
+     */
+    int SearchByCharacter();
+
+    /**
+     * @brief   get a content name from keyboard and display a record with input content name. 
+     * @pre     add data.
+     * @post    content name에 해당하는 레코드를 출력
+     * @return  return 1 if this function works well,
+     *          oterwise return 0.
+     */
+    int SearchAllContentByCharacter(ItemType& data);
+
+    /**
+     * @brief   Display all records in the list in screen.
+     * @pre     None.
+     * @post    None.
+     */
+    void DisplayAllContent();
+
+    /**
+     * @brief	Open a file by file descriptor as an input file.
+     * @pre     a file for reading is exist
+     * @post    open the file for reading
+     * @param   fileName    a filename to open for reading.
+     * @return return 1 if this funcion works well,
+     *          oterwise return 0.
+     */
+    int OpenInFile(char* fileName);
+
+	/**
+     * @brief	Open a file by file descriptor as an output file.
+     * @pre	    list should be initialized.
+     * @post	open the file for writing.
+     * @param	fileName	a filename to open for writing.
+     * @return	return 1 if this function works well,
+     *          otherwise return 0.
+     */
+    int OpenOutFile(char* fileName);
+
+	/**
+     * @brief	Open a file as a read mode, read all data on the file, and set list by the data.
+     * @pre	    The file is not opened.
+     * @post	list holds all records from the file.
+     * @return	return 1 if this function works well,
+     *          otherwise return 0.
+     */
+    int ReadDataFromFile();
+
+	/**
+     * @brief	Open a file as a write mode, and write all data into the file,
+     * @pre	    The file is not opened.
+     * @post	the list is stored in the output file.
+     * @return	return 1 if this function works well,
+     *          otherwise return 0.
+     */
+    int WriteDataToFile();
+};
---
+++ Lab02/SortedList.h
@@ -0,0 +1,120 @@
+#pragma once
+#include <iostream>
+#include <fstream>	
+#include <string>
+using namespace std;
+
+#include "ItemType.h"
+
+#define MAXSIZE 10
+
+// array based simple sorted list.
+class SortedList{
+private:
+	ItemType	m_array[MAXSIZE];		// Array
+	int			m_length;				// Number of Record in list.
+	int			m_curPointer;			// Iterator pointer.
+
+public:
+	// default constructor
+	SortedList() {
+		m_length = 0;
+		ResetList();
+	}
+
+	// default destrctor.
+	~SortedList() {}
+
+	/**
+	 * @brief   Make list empty.
+	 * @pre     None.
+	 * @post    Clear list.
+	 */
+	void MakeEmpty();
+
+	/**
+	 * @brief   Get a number of records in current list.
+	 * @pre     None.
+	 * @post    None.
+	 * @return  Number of record in current list.
+	 */
+	int GetLength() const;
+
+	/**
+	 * @brief   Check capacity of list is full
+	 * @pre     None.
+	 * @post    None.
+	 * @return  Return true if list capacity reached to the upper bound,
+	 * 			Oterwise return false.
+	 */
+	bool IsFull() const;
+
+	/**
+	 * @brief   Initialize list iterator.
+	 * @pre     List should be initialized.
+	 * @post    Iterator is reset.
+	 */
+	void ResetList();
+
+	/**
+	 * @brief   Move list iterator to the next item in list and get the item.
+	 * @pre     List and list iterator should not be initialized.
+	 * @post    Iterator is moved to next item.
+	 * @param   _item    get current iterator's item. it does not need to be initialized.
+	 * @return  index of current iterator's item if is not end of list
+	 * 			oterwise return -1.
+	 */
+	int GetNextItem(ItemType& _item);
+
+	/**
+	 * @brief   Add a new record into sorted list.
+	 * @pre     List should be initialized.
+	 * @post    Added the new record into the sorted list.
+	 * @param   _data    New Item
+	 * @return  Return 1 if this function works well,
+	 *          Return 0 if a record already exists,
+	 * 			Return -1 if list is full.
+	 */
+	int Add(ItemType _data);
+
+	/**
+	 * @brief   Delete existing a record.
+	 * @pre     Two ItemType should be initialized.
+	 * @post    Delete the record into the list and m_length--.
+	 * @param   _item    Delete Item.
+	 * @return  Return 1 if the item to be deleted is in the list,
+	 * 			Return -1 if list is empty,
+	 * 			Oterwise return 0.
+	 */
+	int Delete(ItemType _item);
+
+	/**
+	 * @brief   Searching for same Primary Key by Sequential search and Get the data.
+	 * @pre     ItemType should be initialized.
+	 * @post    Get a record.
+	 * @param   _item    Get current iterator's item.
+	 * @return  Return 1 if record with same Primary Key is set,
+	 * 			Otherwise return 0.
+	 */
+	int Retrieve_SeqS(ItemType& _item);
+
+	/**
+	 * @brief   Searching for same Primary Key by Binary search and Get the data.
+	 * @pre     ItemType should be initialized.
+	 * @post    Get a record.
+	 * @param   _item    Get current iterator's item.
+	 * @return  Return 1 if record with same Primary Key is set,
+	 * 			Otherwise return 0.
+	 */
+	int Retrieve_BinS(ItemType& _item);
+
+	/**
+	 * @brief   Searching for same record and replace record with input record.
+	 * @pre     Two ItemType should be initialized.
+	 * @post    None.
+	 * @param   _item    Replace Record
+	 * @return  Return 1 if two record are the same,
+	 * 			Oterwise return 0.
+	 */
+	int Replace(ItemType _item);
+};
