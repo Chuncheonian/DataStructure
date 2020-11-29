87b8a00d22c6e0d06ea14ac40db12129ae9fff0e
Chuncheonian <ehddud2468@khu.ac.kr>
2020-11-29 16:17:30 +0900

    Add BinarySearchTree

---
+++ [7]BinarySearchTree/main.cpp
@@ -0,0 +1,14 @@
+﻿/**
+ * @mainpage  BinarySearchTree
+ *            This is a simple example of Lab07 on data structures course.<br>
+ * @date      2020.11.29
+ * @author    DongYoung Kwon
+ */
+#include "Application.h"
+
+int main() {
+  Application app;
+  app.Run();
+
+  return 0;
+}
